f146fdf33477a6697f2d41d6afd0713520e9a9a1
Sam Old <siold@malaghan.org.nz>
2023-07-20 16:14:46 +1200

    added minor documentation

--- R/flowjotter.R
+++ R/flowjotter.R
@@ -1,4 +1,4 @@
-#' Flow Jotter Shiny App
+#' Flow Jotter Shiny App Wrapper
 #'
 #' Use this wrapper function to run the shiny app in your RStudio Viewer, or favourite browser. No data needs to be provided to flowjotter until the shiny app is live, and data is stored locally on your machine temporarily.
 #'
